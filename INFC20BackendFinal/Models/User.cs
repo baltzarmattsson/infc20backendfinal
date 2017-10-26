3b11156ca683ba24a3e5c98d7281a2cab11a69a8
Baltzar Mattsson (work) <baltzar.mattsson@wagnerwayfinder.com>
2017-10-26 09:09:47 +0200

    astuasd

--- INFC20BackendFinal/Models/User.cs
+++ INFC20BackendFinal/Models/User.cs
@@ -11,14 +11,16 @@ namespace INFC20BackendFinal.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
+        public string Password { get; set; }
 
         public User() { }
-        public User(string email, string firstName, string lastName, string address)
+        public User(string email, string firstName, string lastName, string address, string password)
         {
             this.Email = email;
             this.FirstName = firstName;
             this.LastName = lastName;
             this.Address = address;
+            this.Password = password;
         }
     }
 }
